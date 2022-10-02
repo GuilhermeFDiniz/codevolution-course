e9309a8c9a4e7494d1d90b20bdb27bf9f5c96569
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-02 12:01:18 -0300

    change the js syntax into jsx syntax of function hello

--- hello-world/src/components/Hello.js
+++ hello-world/src/components/Hello.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const Hello = () => {
-  return(
-    <div>
-      <h1>Hello Guilherme</h1>
-    </div>
-  )
+  // return(
+  //   <div className="lol">
+  //     <h1>Hello Guilherme</h1>
+  //   </div>
+  // )
+  return React.createElement(
+    'div',
+    {id: 'hello',
+     className: 'lol'},
+    React.createElement('h1', null, 'Hello Guilherme'))
 }
 
 export default Hello;
