511f0a93d3ab23a90ebfa7e043d75b42da230a1e
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-03 14:32:15 -0300

    Introduce State into Message class

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,17 +3,15 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from "./components/Welcome"
 import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
       <Greet name="Bruce" heroName="Batman"><p>This is Batman Children</p></Greet>
-      <Greet name="Clark" heroName="Superman"><button>Action</button></Greet>
-      <Greet name="Diana" heroName="Wonderwoman"/>
       <Welcome name="Bruce" heroName="Batman"/>
-      <Welcome name="Clark" heroName="Superman"/>
-      <Welcome name="Diana" heroName="Wonderwoman"/>
       <Hello/>
+      <Message/>
     </div>
   );
 }
