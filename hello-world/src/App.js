5fc2e233424c59cae95d4a4feab451ea266003dd
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-03 14:54:12 -0300

    SetState using callback function and previous state

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,14 +4,16 @@ import Greet from './components/Greet';
 import Welcome from "./components/Welcome"
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman"><p>This is Batman Children</p></Greet>
+      {/* <Greet name="Bruce" heroName="Batman"><p>This is Batman Children</p></Greet>
       <Welcome name="Bruce" heroName="Batman"/>
       <Hello/>
-      <Message/>
+      <Message/> */}
+      <Counter/>
     </div>
   );
 }
