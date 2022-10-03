55838efe25d6d49b04c5f85a1f64c5e8279671ef
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-03 15:45:36 -0300

    Add Event handler on class

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -6,11 +6,13 @@ import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
       <FunctionClick/>
+      <ClassClick/>
       {/* <Greet heroName="Batman" name="Bruce" ><p>This is Batman Children</p></Greet> */}
       {/* <Welcome name="Bruce" heroName="Batman"/> */}
       {/* <Hello/> */}
