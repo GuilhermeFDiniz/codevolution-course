4b9ce5f2b7a2c017aa534c15542bdb37b0ae4976
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-04 16:10:36 -0300

    Pass method from parent to child as props

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -8,13 +8,15 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
+      <ParentComponent/>
       {/* <FunctionClick/> */}
       {/* <ClassClick/> */}
-      <EventBind/>
+      {/* <EventBind/> */}
       {/* <Greet heroName="Batman" name="Bruce" ><p>This is Batman Children</p></Greet> */}
       {/* <Welcome name="Bruce" heroName="Batman"/> */}
       {/* <Hello/> */}
