b720720628c9726b8694e6aac17ce56f8814ba02
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-06 17:52:23 -0300

    Display a list of names as a variable holding the html

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -1,12 +1,32 @@
 import React from 'react'
 
 function NameList() {
+  const persons = [
+    {
+      id: 1,
+      name: 'Bruce',
+      age: 30,
+      skill: 'React'
+    },
+    {
+      id: 2,
+      name: 'Clark',
+      age: 25,
+      skill: 'Angular'
+    },
+    {
+      id: 3,
+      name: 'Diana',
+      age: 28,
+      skill: 'Vue'
+    },
+  ]
   const names = ['Bruce', 'Clark', 'Diana']
+  const nameList = names.map(name => <h2>{name}</h2>)
   return (
     <div>
       {
-        names.map(name =>
-           <h2>{name}</h2>)
+        nameList
       }
     </div>
   )
