1dc04563199acee38c818e8a73e6f2b5a846fed8
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-08 15:27:06 -0300

    Using index as a key

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
+  const names = ['Bruce', 'Clark', 'Bruce','Diana' ]
   const persons = [
     {
       id: 1,
@@ -23,9 +24,9 @@ function NameList() {
     },
   ]
   const personList = persons.map(person => <Person key={person.id} person={person}/>)
-  // const nameList = names.map(name => <h2>{name}</h2>)
+  const nameList = names.map((name, index) => <h2 key={index}>{index} - {name}</h2>)
   return (
-    <div>{personList}</div>
+    <div>{nameList}</div>
   )
 }
 
