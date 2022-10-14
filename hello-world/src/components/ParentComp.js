6bfddb68e3c702253c5be1f9ba75fa9f0a2a060c
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-14 12:39:34 -0300

    Pass props from parent to reg and pure component

--- hello-world/src/components/ParentComp.js
+++ hello-world/src/components/ParentComp.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import PureComp from './PureComp';
+import RegComp from './RegComp';
 
 class ParentComp extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+       name: 'Guilherme'
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        name: "Guilherme"
+      })
+    }, 2000);
+  }
+
   render() {
     return (
       <div>
         Parent Component
+        <RegComp name={this.state.name}/>
+        <PureComp name={this.state.name}/>
       </div>
     )
   }
