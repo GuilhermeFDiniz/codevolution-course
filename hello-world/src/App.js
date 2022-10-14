026d0751237c200a2cb7da1debc5f223f2ca0019
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-14 10:56:41 -0300

    Lifecycle Mounting

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,41 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from "./components/Welcome"
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyles.css'
-import styles from './appStyles.module.css'
-import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
 
 function App() {
   return (
     <div className="App">
-      <Form/>
-      {/* <h1 className='error'>Error</h1> */}
-      {/* <h1 className={styles.success}>Sucess</h1> */}
-      {/* <Inline/> */}
-      {/* <Stylesheet primary={true}/> */}
-      {/* <NameList/> */}
-      {/* <UserGreeting/> */}
-      {/* <ParentComponent/> */}
-      {/* <FunctionClick/> */}
-      {/* <ClassClick/> */}
-      {/* <EventBind/> */}
-      {/* <Greet heroName="Batman" name="Bruce" ><p>This is Batman Children</p></Greet> */}
-      {/* <Welcome name="Bruce" heroName="Batman"/> */}
-      {/* <Hello/> */}
-      {/* <Message/> */}
-      {/* <Counter/> */}
+      <LifecycleA/>
     </div>
   );
 }
---
+++ hello-world/src/components/LifecycleA.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import LifecycleB from './LifecycleB'
+
+class LifecycleA extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+       name: 'Guilherme'
+    }
+    console.log('LifecycleA constructor')
+  }
+
+  static getDerivedStateFromProps(props, state){
+    console.log('LifecycleA getDerivedStateFromProps')
+    return null
+  }
+
+  componentDidMount(){
+    console.log('LifecycleA DidMount')
+  }
+
+  render() {
+    console.log('LifecycleA render')
+    return (
+      <div>
+        <div>
+          Lifecycle A
+        </div>
+        <LifecycleB/>
+      </div>
+    )
+  }
+}
+
+export default LifecycleA
