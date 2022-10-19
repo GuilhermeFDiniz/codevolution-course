63efc5ef7a30d9a79163032ca2f7691e3538d839
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-18 21:28:37 -0300

    Creates Click Counter

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,27 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import ErrorBoundary from './components/ErrorBoundary';
-import Hero from './components/Hero';
-
-
-
-
-
+import ClickCounter from './components/ClickCounter';
 
 function App() {
   return (
     <div className="App">
-      <ErrorBoundary>
-        <Hero heroName="Batman"/>
-      </ErrorBoundary>
-
-      <ErrorBoundary>
-        <Hero heroName="Superman"/>
-      </ErrorBoundary>
-
-      <ErrorBoundary>
-        <Hero heroName="Joker"/>
-      </ErrorBoundary>
+     <ClickCounter/>
     </div>
   );
 }
