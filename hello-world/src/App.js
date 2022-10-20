ccc801577272b2d661d02e774ad3a750db4dbc87
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-20 14:34:43 -0300

    Render Props passing component as a props

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import ClickCounter from './components/ClickCounter';
 import ClickCounterTwo from './components/ClickCounterTwo';
-import HoverCounter from './components/HoverCounter';
+import CounterThree from './components/CounterThree';
 import HoverCounterTwo from './components/HoverCounterTwo';
+import User from './components/User';
 
 function App() {
   return (
     <div className="App">
-     <ClickCounterTwo/>
+      <CounterThree render={(count, incrementCount)=> <ClickCounterTwo count={count} incrementCount={incrementCount}/>}/>
+      <CounterThree render={(count, incrementCount)=> <HoverCounterTwo count={count} incrementCount={incrementCount}/>}/>
+     {/* <ClickCounterTwo/>
      <HoverCounterTwo/>
+     <User render={ (isLoggedIn) =>  isLoggedIn ? 'Guilherme' : 'Guest'}/> */}
     </div>
   );
 }
