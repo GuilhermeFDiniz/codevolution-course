88065ba8ed1d792cab655042421ea42cb5963399
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-20 12:39:52 -0300

    Render Props

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import ClickCounter from './components/ClickCounter';
+import ClickCounterTwo from './components/ClickCounterTwo';
 import HoverCounter from './components/HoverCounter';
+import HoverCounterTwo from './components/HoverCounterTwo';
 
 function App() {
   return (
     <div className="App">
-     <ClickCounter name="Guilherme"/>
-     <HoverCounter />
+     <ClickCounterTwo/>
+     <HoverCounterTwo/>
     </div>
   );
 }
