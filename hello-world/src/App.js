7ea9014d40e3c470348d5b94a109c582a94f987b
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-21 14:53:07 -0300

    Pass nested components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import ClickCounterTwo from './components/ClickCounterTwo';
-import CounterThree from './components/CounterThree';
-import HoverCounterTwo from './components/HoverCounterTwo';
-import User from './components/User';
+import ComponentC from './components/ComponentC';
 
 function App() {
   return (
     <div className="App">
-      <CounterThree>
-        {(count, incrementCount)=> <ClickCounterTwo count={count} incrementCount={incrementCount}/>}
-        </CounterThree>
-      <CounterThree>
-      {(count, incrementCount)=> <HoverCounterTwo count={count} incrementCount={incrementCount}/>}
-      </CounterThree>
-     {/* <ClickCounterTwo/>
-     <HoverCounterTwo/>
-     <User render={ (isLoggedIn) =>  isLoggedIn ? 'Guilherme' : 'Guest'}/> */}
+      <ComponentC/>
     </div>
   );
 }
