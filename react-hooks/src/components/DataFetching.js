5b103119cd927f837b262602c1f58c703590d192
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-26 10:08:13 -0300

    Fetch data with useEffect passing a button watch

--- react-hooks/src/components/DataFetching.js
+++ react-hooks/src/components/DataFetching.js
@@ -4,18 +4,24 @@ import axios from 'axios'
 function DataFetching() {
   const [post, setPost] = useState({})
   const [id, setId] = useState(1)
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1)
 
   useEffect(() => {
     axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`).then(response => {
     console.log(response.data)
     setPost(response.data)
     }).catch(error => console.log(error))
-  },[id])
+  },[idFromButtonClick])
+
+  const handleClick = () => {
+    setIdFromButtonClick(id)
+  }
 
   return (
     <div>
       <input type="text" value={id} onChange={e => setId(e.target.value)}></input>
       <div>{post.title}</div>
+      <button type="button" onClick={handleClick}>Fetch Post</button>
       {/* <ul>
         {
           posts.map(post => <li key={post.id}>{post.title}</li>)
