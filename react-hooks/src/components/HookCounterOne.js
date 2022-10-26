65c4f6c380a50fd888d584af16022449d59fe598
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-26 00:36:43 -0300

    Conditional updating useEffect

--- react-hooks/src/components/HookCounterOne.js
+++ react-hooks/src/components/HookCounterOne.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react'
 
 function HookCounterOne() {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
 
   useEffect(() => {
+    console.log('Updating document title')
     document.title = `You clicked ${count} times`
-  })
+  }, [count])
 
   return (
     <div>
+      <input value={name} onChange={e => setName(e.target.value)}></input>
       <button onClick={() => setCount(count+1)}>Click {count}</button>
     </div>
   )
