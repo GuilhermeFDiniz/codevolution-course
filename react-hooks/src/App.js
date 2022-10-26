769e0677185e574a94deda688c2b1652d2d77e66
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-26 19:08:17 -0300

    Counter using previous state in Hook

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
+
 
 function App() {
   return (
     <div className="App">
-    <ClassCounter/>
-    <HookCounter/>
+      <HookCounterTwo/>
     </div>
   );
 }
