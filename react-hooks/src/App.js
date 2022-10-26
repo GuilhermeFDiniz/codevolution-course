4b260d1208956623a71b62f50b95bcc6227787e3
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-26 15:23:01 -0300

    Fetch data with useReducer

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,16 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import CounterOne from './components/CounterOne';
-import CounterTwo from './components/CounterTwo';
-import CounterThree from './components/CounterThree';
+import DataFetchingOne from './components/DataFetchingOne';
+
 
 
 function App() {
   return (
     <div className="App">
-      {/* <CounterOne/> */}
-      {/* <CounterTwo/> */}
-      <CounterThree/>
+      <DataFetchingOne/>
     </div>
   );
 }
