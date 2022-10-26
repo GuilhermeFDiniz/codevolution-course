af2ab7f41833c3acbde5be752d52fc23892476b1
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-26 00:27:36 -0300

    Update document title as a useEffect

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import HookCounterFour from './components/HookCounterFour';
-
+import HookCounterOne from './components/HookCounterOne';
 
 
 function App() {
   return (
     <div className="App">
-      <HookCounterFour/>
+      <HookCounterOne/>
     </div>
   );
 }
