af3e984a43857e5e23e5eb940c863695fa6a9b31
Guilherme Diniz <guidinizeng@gmail.com>
2022-10-26 15:35:12 -0300

    Fetch data with useReducer and useEffect

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
 
 
 
 function App() {
   return (
     <div className="App">
-      <DataFetchingOne/>
+      {/* <DataFetchingOne/> */}
+      <DataFetchingTwo/>
     </div>
   );
 }
