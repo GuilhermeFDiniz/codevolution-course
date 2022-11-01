6e65d93944b0a66b207ad706c702a4d938fe7ee5
Guilherme Diniz <guidinizeng@gmail.com>
2022-11-01 12:00:06 -0300

    Render just Child

--- react-render/src/components/Parent Child/Child.js
+++ react-render/src/components/Parent Child/Child.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Child = () => {
+  const [count, setCount] = useState(0)
   console.log('Child Render')
   return (
     <div>
       Child Component
+      <button onClick={() => setCount(c => c +1)}> Count - {count}</button>
+      <button onClick={() => setCount(0)}>Count to 0</button>
+      <button onClick={() => setCount(5)}>Count to 5</button>
     </div>
   )
 }
