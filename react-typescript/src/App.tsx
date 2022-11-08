000797a72c56041316c8ee34b1223384559ec4be
Guilherme Diniz <guidinizeng@gmail.com>
2022-11-08 16:48:40 -0300

    Declaring option types

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -3,31 +3,13 @@ import './App.css';
 import Greet from './components/Greet';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne'
-  }
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne'
-    },
-    {
-      first: 'Clark',
-      last: 'Kent'
-    },
-    {
-      first: 'Princess',
-      last: 'Diana'
-    }
-  ]
+
   return (
     <div className="App">
-      <Greet name='Guilherme' messageCount={20} isLoggedIn={false}/>
-      <Person name={personName}/>
-      <PersonList names={nameList}/>
+      <Status status={'loading'} />
     </div>
   );
 }
