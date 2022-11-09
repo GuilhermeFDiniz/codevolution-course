c1f27947f2a9b7e022962f4ee3b0ba498f5f4da7
Guilherme Diniz <guidinizeng@gmail.com>
2022-11-09 15:08:18 -0300

    On Change and OnClick props

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,12 +1,14 @@
 
 import './App.css';
 import { Button } from './components/Button'
+import { Input } from './components/Input';
 
 function App() {
 
   return (
     <div className="App">
       <Button handleClick={(event, id) => console.log('Button clicked', event, id)}/>
+    <Input value='' handleChange={(event) => console.log(event)}/>
     </div>
   );
 }
