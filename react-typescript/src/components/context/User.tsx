8f833b49b7e6cdad83d84485c0ab53105f480b76
Guilherme Diniz <guidinizeng@gmail.com>
2022-11-18 13:03:50 -0300

    User Context type

--- react-typescript/src/components/context/User.tsx
+++ react-typescript/src/components/context/User.tsx
@@ -1,12 +1,23 @@
+import { useContext } from "react"
+import { UserContext } from "./UserContext"
+
 export const User = () => {
-  const handleLogin = () => {}
-  const handleLogout = () => {}
+  const userContext = useContext(UserContext)
+  const handleLogin = () => {
+    userContext.setUser({
+      name: 'Guilherme',
+      email: 'guidiniz@gmail.com',
+    })
+  }
+  const handleLogout = () => {
+    userContext.setUser(null)
+  }
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
-      <div>User name is</div>
-      <div>User email is</div>
+      <div>User name is {userContext.user?.name}</div>
+      <div>User email is {userContext.user?.email}</div>
     </div>
   )
 }
