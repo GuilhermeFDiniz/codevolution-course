40600b704afb47ef24e1c8982de08c009ae620a3
Guilherme Diniz <guidinizeng@gmail.com>
2022-12-09 23:58:27 -0300

    Create Pages and Navbar

---
+++ shopping-cart/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
