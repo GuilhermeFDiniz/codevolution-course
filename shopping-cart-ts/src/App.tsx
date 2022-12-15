787296807e7ef05c95eb5e09df0ec0c23ac64d31
Guilherme Diniz <guidinizeng@gmail.com>
2022-12-15 11:02:37 -0300

    Create Routes for About, Home and Store

--- shopping-cart-ts/src/App.tsx
+++ shopping-cart-ts/src/App.tsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
+import { Container } from 'react-bootstrap'
 import './App.css'
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
+import { About } from './pages/About'
 
 function App() {
 
   return (
-    <div className="App">
-      HI
-    </div>
+  <Container className='mb-4'>
+    <Routes>
+      <Route path="/" element={ <Home />}></Route>
+      <Route path="/store" element={ <Store />}></Route>
+      <Route path="/about" element={ <About />}></Route>
+    </Routes>
+  </Container>
   )
+
 }
 
 export default App
