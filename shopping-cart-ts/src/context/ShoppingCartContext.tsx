0aa0327e7e6cb62b41db17df51e9f0d8559095b1
Guilherme Diniz <guidinizeng@gmail.com>
2022-12-20 14:58:34 -0300

    Create decreaseCartQuantity function

--- shopping-cart-ts/src/context/ShoppingCartContext.tsx
+++ shopping-cart-ts/src/context/ShoppingCartContext.tsx
@@ -7,7 +7,7 @@ type ShoppingCartProviderProps = {
 type ShoppingCartcontext = {
   getItemQuantity: (id:number) => number
   increaseCartQuantity: (id:number) => void
-  decreaseCartQuantity: (id:number) => number
+  decreaseCartQuantity: (id:number) => void
   removeFromCart: (id:number) => void
 }
 
@@ -48,11 +48,23 @@ export function ShoppingCartProvider( {children}: ShoppingCartProviderProps ) {
   }
 
   function decreaseCartQuantity(id: number) {
-
+    setCartItems(currentItems => {
+      if(currentItems.find(item => item.id === id)?.quantity === 1){
+        return currentItems.filter(item => item.id !== id)
+      } else {
+        return currentItems.map(item => {
+          if (item.id === id) {
+            return {...item, quantity: item.quantity - 1}
+          } else {
+            return item
+          }
+        })
+      }
+    })
   }
 
   function removeFromCart(id: number) {
-
+   
   }
 
   return <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart}}>
