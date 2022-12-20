4a3527b01353239675506f008540fc38f0f4709e
Guilherme Diniz <guidinizeng@gmail.com>
2022-12-20 14:46:31 -0300

    Create increaseCartQuantity function

--- shopping-cart-ts/src/context/ShoppingCartContext.tsx
+++ shopping-cart-ts/src/context/ShoppingCartContext.tsx
@@ -32,7 +32,19 @@ export function ShoppingCartProvider( {children}: ShoppingCartProviderProps ) {
   }
 
   function increaseCartQuantity(id: number) {
-  
+    setCartItems(currentItems => {
+      if(currentItems.find(item => item.id === id) == null){
+        return [...currentItems, {id, quantity: 1}]
+      } else {
+        return currentItems.map(item => {
+          if (item.id === id) {
+            return {...item, quantity: item.quantity + 1}
+          } else {
+            return item
+          }
+        })
+      }
+    })
   }
 
   function decreaseCartQuantity(id: number) {
