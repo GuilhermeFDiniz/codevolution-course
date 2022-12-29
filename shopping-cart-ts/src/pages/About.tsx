559f0930f5e3180e93eeee655858275d31b76214
Guilherme Diniz <guidinizeng@gmail.com>
2022-12-29 19:01:32 -0300

    About Page

--- shopping-cart-ts/src/pages/About.tsx
+++ shopping-cart-ts/src/pages/About.tsx
@@ -1,3 +1,9 @@
 export function About() {
-  return <h1>About</h1>
+  return (
+    <div>
+      <h1>About</h1>
+      <h4>Hello. I'm Guilherme Diniz, I'm a Software Engineer with experience building web applications. My expertise lies in React.js, Node.js, Typescript and Ruby on Rails. </h4>
+      <h4>This project is for practicing React.</h4>
+    </div>
+  )
 }
