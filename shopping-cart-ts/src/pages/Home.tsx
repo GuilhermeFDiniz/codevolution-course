a9a10dd70e4428e8caaf7539228a900d2d2bf73b
Guilherme Diniz <guidinizeng@gmail.com>
2022-12-30 11:54:02 -0300

    Home Page

--- shopping-cart-ts/src/pages/Home.tsx
+++ shopping-cart-ts/src/pages/Home.tsx
@@ -1,3 +1,12 @@
+import { Card } from "react-bootstrap";
+
 export function Home() {
-  return <h1>Home</h1>
+  return(
+    <div>
+      <h1>Pokemon Store</h1>
+      <Card className="h-100">
+          <Card.Img variant="top" src={"../../public/images/Pokemon.jpeg"} style={{width: "500px", height: "500px"}}   />
+      </Card>
+    </div>
+  )
 }
