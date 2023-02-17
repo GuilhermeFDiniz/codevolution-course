d6fb1e71828e75005c9463848fa12d682f277191
Guilherme Diniz <guidinizeng@gmail.com>
2023-02-17 12:36:58 -0300

    Class Using

--- node-basics/index.js
+++ node-basics/index.js
@@ -1,6 +1,8 @@
-const superHero = require("./super-hero")
-console.log(superHero.getName());
-superHero.setName('Ola')
-console.log(superHero.getName());
-const newSuperHero = require('./super-hero')
-console.log(newSuperHero.getName());
+const SuperHero = require("./super-hero")
+const batman = new SuperHero("Batman")
+console.log(batman.getName());
+batman.setName("Bruce Wayne")
+console.log(batman.getName());
+
+const superman = new SuperHero("Superman")
+console.log(superman.getName());
