254917fdf2d67994af1dd853e569d43348594bb9
Guilherme Diniz <guidinizeng@gmail.com>
2023-02-17 12:34:57 -0300

    Module Cachingr

--- node-basics/index.js
+++ node-basics/index.js
@@ -1,3 +1,6 @@
-require('./batman')
-require('./superman')
-
+const superHero = require("./super-hero")
+console.log(superHero.getName());
+superHero.setName('Ola')
+console.log(superHero.getName());
+const newSuperHero = require('./super-hero')
+console.log(newSuperHero.getName());
