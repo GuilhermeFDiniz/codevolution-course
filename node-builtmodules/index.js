5a08f8168cfc95e270a9d78c29dfd47f0df69e09
Guilherme Diniz <guidinizeng@gmail.com>
2023-02-19 01:06:20 -0300

    Extending from Event Emitter

--- node-builtmodules/index.js
+++ node-builtmodules/index.js
@@ -1,15 +1,28 @@
-const EventEmitter = require('node:events');
+const PizzaShop = require("./pizza-shop");
+const DrinkMachine = require("./drink-machine");
+const pizzaShop = new PizzaShop();
+const drinkMachine = new DrinkMachine();
 
-const emitter = new EventEmitter();
-emitter.on("order-pizza", (size, topping) => {
-  console.log('Order Received! baking a pizza', size, topping);
+pizzaShop.on("order", (size, topping) => {
+  console.log('Order', size, topping);
+  drinkMachine.serveDrink(size)
 });
 
-emitter.on("order-pizza", (size) => {
-  if(size === "large"){
-    console.log('Serving complimentary drink');
-  }
-});
+pizzaShop.order("large", "mushrooms");
+pizzaShop.displayOrderNumber();
 
-console.log('do work');
-emitter.emit("order-pizza", "large", "mushroom")
+// const EventEmitter = require('node:events');
+
+// const emitter = new EventEmitter();
+// emitter.on("order-pizza", (size, topping) => {
+//   console.log('Order Received! baking a pizza', size, topping);
+// });
+
+// emitter.on("order-pizza", (size) => {
+//   if(size === "large"){
+//     console.log('Serving complimentary drink');
+//   }
+// });
+
+// console.log('do work');
+// emitter.emit("order-pizza", "large", "mushroom")
