43510abd622a3606fb8c9367d42d7d79621b7195
Guilherme Diniz <guidinizeng@gmail.com>
2023-02-20 13:56:05 -0300

    HTML Routing

--- node-http/index.js
+++ node-http/index.js
@@ -2,11 +2,22 @@ const http = require("node:http")
 const fs = require("node:fs")
 
 const server = http.createServer((req, res)=>{
-  const name = "Guilherme"
-  res.writeHead(200, {"Content-Type": "text/html"})
-  let html = fs.readFileSync("./index.html", "utf-8")
-  html = html.replace("{{name}}", name)
-  res.end(html)
+  if(req.url === "/"){
+    res.writeHead(200, {"Context-Type": "text/plain"})
+    res.end("Home Page")
+  } else if (req.url === "/about"){
+    res.writeHead(200, {"Context-Type": "text/plain"})
+    res.end("About page")
+  } else if (req.url === "/api"){
+    res.writeHead(200, {"Context-Type": "application/json"})
+    res.end(JSON.stringify({
+      firstName: "Bruce",
+      lastName: "Wayne"
+    }))
+  } else {
+    res.writeHead(404)
+    res.end("Page Not Found Budy")
+  }
 })
 
 server.listen(3000, () => {
