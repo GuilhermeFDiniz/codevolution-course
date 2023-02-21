57c02dc02241c8aa7712cd623cbf086bca923423
Guilherme Diniz <guidinizeng@gmail.com>
2023-02-21 15:08:10 -0300

    Thread Pool Size

--- node-libuv/index.js
+++ node-libuv/index.js
@@ -1,15 +1,16 @@
 const crypto = require("node:crypto")
 
-const MAX_CALLS = 300;
-
+const MAX_CALLS = 30;
+process.env.UV_THREADPOOL_SIZE = 4
 const start = Date.now()
-// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512")
-// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512")
-// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512")
-// console.log("Hash", Date.now() - start);
 
 for (let i = 0; i < MAX_CALLS; i++){
   crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
     console.log(`Hash ${i+1}`, Date.now() - start);
   });
 }
+
+// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512")
+// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512")
+// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512")
+// console.log("Hash", Date.now() - start);
